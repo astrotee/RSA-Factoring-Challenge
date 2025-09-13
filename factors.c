4986b3bf984fc136832a2e117b4f00b71f628727
amin ahmed <aminahmed24@gmail.com>
2025-09-14 02:54:27 +0300

    Fix mpz init

--- factors.c
+++ factors.c
@@ -5,27 +5,32 @@
 
 void factors(mpz_ptr n) {
   size_t counter = 0;
-  mpz_t a, b, b1, b2, rn, tmp;
-  mpz_inits(a, b, b1, b2, rn, tmp);
+  mpz_ptr a, b = NULL;
+  mpz_t b1, b2, rn, tmp;
+  mpz_inits(b1, b2, rn, tmp, NULL);
   mpz_sqrt(rn, n);
   mpz_set_ui(b1, 2);
   mpz_set(b2, rn);
   for (; counter <= 100; counter++) {
     mpz_mod(tmp, n, b1);
-    if (mpz_cmp_ui(tmp, 0)) {
-      mpz_div(a, n, b1);
+    if (mpz_cmp_ui(tmp, 0) == 0) {
+      mpz_div(tmp, n, b1);
+      a = tmp;
+      b = b1;
       break;
     }
     mpz_mod(tmp, n, b2);
-    if (mpz_cmp_ui(tmp, 0)) {
-      mpz_div(a, n, b2);
+    if (mpz_cmp_ui(tmp, 0) == 0) {
+      mpz_div(tmp, n, b2);
+      b = tmp;
+      a = b2;
       break;
     }
     mpz_add_ui(b1, b1, 1);
     mpz_add_ui(b2, b2, 1);
   }
   printf("%lu=%lu*%lu\n", mpz_get_ui(n), mpz_get_ui(a), mpz_get_ui(b));
-  mpz_clears(a, b, b1, b2, rn, tmp);
+  mpz_clears(b1, b2, rn, tmp);
 }
 
 int main(int argc, char *argv[]) {
