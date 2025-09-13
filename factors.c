f4cb6380611e48339c70bae2c7f229b452c5b591
amin ahmed <aminahmed24@gmail.com>
2025-09-14 02:01:12 +0300

    0. Factorize all the things!

---
+++ factors.c
@@ -0,0 +1,53 @@
+#include <gmp.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void factors(mpz_ptr n) {
+  size_t counter = 0;
+  mpz_t a, b, b1, b2, rn, tmp;
+  mpz_inits(a, b, b1, b2, rn, tmp);
+  mpz_sqrt(rn, n);
+  mpz_set_ui(b1, 2);
+  mpz_set(b2, rn);
+  for (; counter <= 100; counter++) {
+    mpz_mod(tmp, n, b1);
+    if (mpz_cmp_ui(tmp, 0)) {
+      mpz_div(a, n, b1);
+      break;
+    }
+    mpz_mod(tmp, n, b2);
+    if (mpz_cmp_ui(tmp, 0)) {
+      mpz_div(a, n, b2);
+      break;
+    }
+    mpz_add_ui(b1, b1, 1);
+    mpz_add_ui(b2, b2, 1);
+  }
+  printf("%lu=%lu*%lu\n", mpz_get_ui(n), mpz_get_ui(a), mpz_get_ui(b));
+  mpz_clears(a, b, b1, b2, rn, tmp);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    return EXIT_FAILURE;
+  }
+  char *filename = argv[1];
+  FILE *file = fopen(filename, "r");
+  if (file == NULL) {
+    perror("Error opening file");
+    return EXIT_FAILURE;
+  }
+  char *line = NULL;
+  size_t size = 0;
+  mpz_t n;
+  mpz_init(n);
+  while (getline(&line, &size, file) != -1) {
+    mpz_set_str(n, line, 10);
+    factors(n);
+  }
+  mpz_clear(n);
+  free(line);
+  fclose(file);
+  return EXIT_SUCCESS;
+}
